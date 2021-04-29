d343194b06c3e330e7a278d1d6dcaaf2206def7a
deszus <68805470+deszus@users.noreply.github.com>
2021-04-29 10:20:26 +0200

    1st upload :D

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,17 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+        int result = 0;
+        if (input == null || input.length() < 1) {
+            return -1;
+        }
+        for (char letter : input.toCharArray()) {
+            if (letter > 47 && letter < 58) {
+                result += letter - 48;
+            }
+        }
+        return result;
+    }
 }
